f1d8d275966cc43300ab7c44d62dce2c80aca302
José Medeiros <j.edil@ene.unb.br>
2018-03-23 17:21:19 -0300

    Add UCS setup for 25MHz operation

--- src/usc/usc.c
+++ src/usc/usc.c
@@ -16,15 +16,14 @@ disable_watchdog (void)
 // To achieve maximum performance, we should increase the supply voltage.
 // This is done by means of the Power Management Module (PMM).
 // Please refer to the device datasheet and family guide (slau208q) for details.
-// Warning: potentially unsafe, do not use this in production systems!
+// Warning: potentially unsafe, check it before use in production systems!
 uint16_t setVCoreUp (uint8_t level){
     uint32_t PMMRIE_bak, SVSMHCTL_bak, SVSMLCTL_bak;
 
     //Open PMM registers for write access
     PMMCTL0_H = 0xA5;
 
-    //Disable dedicated Interrupts
-    //Backup all registers
+    //Disable interrupts and backup registers
     PMMRIE_bak = PMMRIE;
     PMMRIE &= ~(SVMHVLRPE | SVSHPE | SVMLVLRPE |
 		SVSLPE | SVMHVLRIE | SVMHIE |
@@ -34,7 +33,7 @@ uint16_t setVCoreUp (uint8_t level){
     SVSMHCTL_bak = SVSMHCTL;
     SVSMLCTL_bak = SVSMLCTL;
 
-    //Clear flags
+    //Clear interrupt flags
     PMMIFG = 0;
 
     //Set SVM highside to new level and check if a VCore increase is possible
@@ -42,13 +41,11 @@ uint16_t setVCoreUp (uint8_t level){
 
     //Wait until SVM highside is settled
     while ((PMMIFG & SVSMHDLYIFG) == 0) ;
-
-    //Clear flag
     PMMIFG &= ~SVSMHDLYIFG;
 
     //Check if a VCore increase is possible
     if (PMMIFG & SVMHIFG == SVMHIFG){
-        //-> Vcc is too low for a Vcore increase
+        // Vcc is too low for a Vcore increase
         //recover the previous settings
         PMMIFG &= ~SVSMHDLYIFG;
         SVSMHCTL = SVSMHCTL_bak;
@@ -70,14 +67,12 @@ uint16_t setVCoreUp (uint8_t level){
         return ( -1 ) ;
     }
 
-    //Set also SVS highside to new level
     //Vcc is high enough for a Vcore increase
+    //Set also SVS highside to new level
     SVSMHCTL |= (SVSHRVL0 * level);
 
     //Wait until SVM highside is settled
     while (PMMIFG & SVSMHDLYIFG == 0) ;
-
-    //Clear flag
     PMMIFG &= ~SVSMHDLYIFG;
 
     //Set VCore to new level
@@ -92,6 +87,7 @@ uint16_t setVCoreUp (uint8_t level){
 
     //Clear flag
     PMMIFG &= ~SVSMLDLYIFG;
+
     //SVS, SVM core and high side are now set to protect for the new core level
 
     //Restore Low side settings
@@ -139,6 +135,7 @@ uint16_t setVCoreUp (uint8_t level){
     return ( 0 ) ;
 }
 
+// Ramps up Vcore voltage to desired level.
 uint8_t setVCore (uint8_t level){
     uint8_t actlevel;
     uint8_t status = 0;
@@ -150,13 +147,12 @@ uint8_t setVCore (uint8_t level){
     //Get actual VCore
     actlevel = (PMMCTL0 & PMMCOREV_3);
 
-    //Disable interrupts because certain peripherals will not
-    //work during VCORE change
+    //Disable interrupts
     interruptState = __get_interrupt_state();
     __disable_interrupt();
     __no_operation();
 
-    //step by step increase or decrease
+    // Ramp voltage up
     while ((level != actlevel) && (status == 0))
     {
         if (level > actlevel){
@@ -175,10 +171,51 @@ uint8_t setVCore (uint8_t level){
     return ( status) ;
 }
 
+// Configure the Unified Clock System
+void initClock(void)
+{
+  UCSCTL3 = SELREF_2;		   // Set DCO FLL reference = REFO
+
+  __bis_SR_register(SCG0);	   // Disable the FLL control loop
+
+  UCSCTL0 = 0x0000;		   // Set lowest possible DCOx, MODx
+                                   // FLL later adjusts this value.
+
+  UCSCTL1 = DCORSEL_7;		   // Select DCO range 50MHz operation
+  UCSCTL2 = FLLD_0 + 762;	   // Set DCO Multiplier for 25MHz
+				   // (N + 1) * FLLRef = Fdco
+				   // (762 + 1) * 32768 = 25MHz
+				   // Set FLL Div = fDCOCLK/1
+  __bic_SR_register(SCG0);	   // Enable the FLL control loop
+
+  // Worst-case settling time for the DCO when the DCO range bits have been
+  // changed is n x 32 x 32 x f_MCLK / f_FLL_reference. See UCS chapter in 5xx
+  // UG for optimization.
+  // 32 x 32 x 25 MHz / 32,768 Hz ~ 780k MCLK cycles for DCO to settle
+  __delay_cycles(782000);
+
+  // Loop until XT1,XT2 & DCO stabilizes - In this case only DCO has to stabilize
+  do
+  {
+    UCSCTL7 &= ~(XT2OFFG + XT1LFOFFG + DCOFFG);
+				   // Clear XT2,XT1,DCO fault flags
+    SFRIFG1 &= ~OFIFG;		   // Clear fault flags
+  } while (SFRIFG1&OFIFG);	   // Test oscillator fault flag
+
+  // Set clock sources
+  UCSCTL4 |=
+    SELA_0 +		   // ACLK = XT1CLK = 32.768kHz (high precision)
+    SELS_4 +		   // SMCLK = DCOCLKDIV = 25MHz
+    SELM_4;		   // MCLK = DCOCLKDIV = 25MHz
+}
+
 
 
 void main(void)
 {
+  // Increase Vcore to maximum performance
+  setVCore(0x3);
+
   P4DIR |= BIT7; 	// Output pin.
 
   while(1){
